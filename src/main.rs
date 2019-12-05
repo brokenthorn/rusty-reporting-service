c905dd5aec734e74d82b82c2dbe75acf724a8ed3
Paul-Sebastian Manole <brokenthorn@gmail.com>
2019-12-05 22:07:32 +0200

    Iterate over all SQL result columns

--- src/main.rs
+++ src/main.rs
@@ -37,42 +37,41 @@ fn main() {
     }
 
     let mut manager = SimpleManager::new();
-    let i: Interval = 2.second();
+    let i: Interval = 5.second();
 
     manager.add_task(i, move || {
-        let s = span!(Level::INFO, LOG_SPAN_NAME);
+        let s = span!(Level::INFO, "SQL_REPORT_1");
         let _guard = s.enter();
         // TODO: Here check if current day is day 1 of Month,
         //  because we want to run once every Month on day 1.
-        event!(Level::INFO, msg = "A job was triggered.");
+        event!(Level::INFO, msg = "SQL Server Report Job 1 was triggered.");
 
-        let mut connection_string: String = String::new();
-
-        if cfg!(debug_assertions) {
-            connection_string.push_str("server=tcp:10.0.0.140,1433;integratedSecurity=false;TrustServerCertificate=true;username=sa;password=REPLACE_WITH_PASSWORD");
+        let connection_string = if cfg!(debug_assertions) {
             event!(Level::WARN, msg = "USING A HARD-CODED `SQL_SERVER_CONNECTION_STRING`!");
+            String::from("server=tcp:10.0.0.140,1433;integratedSecurity=false;TrustServerCertificate=true;username=sa;password=REPLACE_WITH_PASSWORD")
         } else {
             // Get connection string from environment variable:
             event!(Level::INFO, msg="Using value from `SQL_SERVER_CONNECTION_STRING` environment variable.");
-            connection_string = std::env::var("SQL_SERVER_CONNECTION_STRING").expect("SQL_SERVER_CONNECTION_STRING environment variable is missing!");
-        }
+            std::env::var("SQL_SERVER_CONNECTION_STRING").expect("SQL_SERVER_CONNECTION_STRING environment variable is missing!")
+        };
 
         let sql_future = SqlConnection::connect(connection_string.as_str()).and_then(|conn| {
-            let s = span!(Level::INFO, LOG_SPAN_NAME);
-            let _guard = s.enter();
-
             conn
                 .query("SELECT x FROM (VALUES (1), (2), (3), (4)) numbers(x) WHERE x % @P1 = @P2", &[&2i32, &0i32])
                 .for_each(|row| {
-                    let val: i32 = row.get(0);
-                    event!(Level::INFO, msg = "Got value", val);
+                    let s = span!(Level::INFO, "QUERY_RESULT");
+                    let _guard = s.enter();
+                    for column_index in 0..row.len() {
+                        let value: i32 = row.get(column_index);
+                        event!(Level::INFO, msg = format!("column={}, value={:?}", column_index, value).as_str());
+                    }
                     Ok(())
                 })
         }).map_err(|e: tiberius::Error| {
             event!(Level::ERROR, error_msg = format!("{:?}", e).as_str());
         });
 
-        event!(Level::INFO, msg = "Spinning up a Tokio executor on the current thread, to connect to SQL server and run queries.");
+        event!(Level::INFO, msg = "Spinning up a Tokio executor on the current thread in order to run SQL query.");
 
         match tokio::runtime::current_thread::block_on_all(sql_future) {
             Ok(_) => event!(Level::INFO, msg = "Tokio executor finished running all futures successfully."),
@@ -81,7 +80,7 @@ fn main() {
     });
 
     // the interval set here basically sets the fidelity of the task scheduler used within the manager
-    manager.start(Duration::from_millis(100));
+    manager.start(Duration::from_millis(500));
 
     event!(Level::INFO, "Application exited.");
 }
